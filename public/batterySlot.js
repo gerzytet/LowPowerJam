288668aded598481425ec127bc68ea5dea1c0442
Craig White <gerzytet@gmail.com>
2022-10-08 12:03:30 -0400

    Add missing file

---
+++ public/batterySlot.js
@@ -0,0 +1,25 @@
+const BATTERY_SLOT_SIZE = 10
+const BATTERY_SLOT_RADIUS = 20
+
+class BatterySlot {
+    constructor(pos, hasBattery) {
+        this.pos = pos
+        this.hasBattery = hasBattery
+    }
+
+    render() {
+        push()
+            translate(this.pos)
+            if (this.hasBattery) {
+                fill(0, 255, 0)
+            } else {
+                fill(0, 0, 0)
+            }
+            box(BATTERY_SLOT_SIZE)
+        pop()
+    }
+
+    getCollider() {
+        return new SphereCollider(this.pos.copy(), BATTERY_SLOT_RADIUS)
+    }
+}
