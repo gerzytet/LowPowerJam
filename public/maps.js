e2828296d91351384eeeafcbb327699e8871d318
Craig White <gerzytet@gmail.com>
2022-10-09 01:18:35 -0400

    merge maps

--- public/maps.js
+++ public/maps.js
@@ -19,25 +19,6 @@ function cv2(x, y) {
 function initMaps() {
     const GROUND = -50
 
-<<<<<<< HEAD
-    maps.push({
-        playerSpawn: createVector(100, GROUND, 100),
-        name: "Basic map",
-        objects: [
-            new Wall(cv2(-100, -100), cv2(600, -100)),
-            new Wall(cv2(-100, -100), cv2(-100, 600)),
-            new Wall(cv2(600, -100), cv2(600, 600)),
-            new Wall(cv2(-100, 600), cv2(600, 600)),
-
-            new HealthKitchen(cv(0, 0), new BatterySlot(cv(100, 100), false), 0),
-            new HealthKitchen(cv(500, 500), new BatterySlot(cv(400, 400), false), 0),
-            new TomatoKitchen(cv(500, 0), new BatterySlot(cv(400, 100), true), 1),
-            new TomatoKitchen(cv(0, 500), new BatterySlot(cv(100, 400), true), 1),
-
-            new Wall(cv2(250, 50), cv2(250, 450)),
-            new Wall(cv2(50, 250), cv2(450, 250))
-        ]
-=======
     maps.push(
         {
             playerSpawn: createVector(100, GROUND, 100),
@@ -56,7 +37,6 @@ function initMaps() {
                 new Wall(cv2(800, 0), cv2(1200, 400)),
                 new Wall(cv2(0, 800), cv2(400, 1200))
             ],
->>>>>>> 86156f0770742ae4b74b7baa4d2898cb07bde546
     })
 
     maps.push(
